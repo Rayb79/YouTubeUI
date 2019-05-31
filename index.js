ac760f5d2eab7996e49f9140fdb0256dd3edf88f
Ramaun Bowie <ramaunb@hotmail.com>
2019-05-31 17:03:55 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as YouTube} from './app.json';
+
+AppRegistry.registerComponent(YouTube, () => App);
